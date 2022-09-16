9a073a8e54a6a3a2446d1c74a825ec0b01c948a6
viraj0076 <20it057@charusat.edu.in>
2022-09-16 12:58:41 +0530

    Added Alert Toast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import Student from './components/Student';
-import StudentList from './components/StudentList';
-import NavigationBar from './components/NavigationBar';
-import Footer from './components/Footer';
-import Container from 'react-bootstrap/Container';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import "./App.css";
+import Student from "./components/Student";
+import StudentList from "./components/StudentList";
+import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyAlert from "./components/MyAlert";
+import { useState } from "react";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+
+  let showAlert = (type, message) => {
+    setAlert({
+      type: type,
+      message: message,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
 
   return (
     <div className="App">
       <Router>
-      <NavigationBar/>
-      <Container>
-        <Routes>
-      <Route path="student" element={<Student/>}/>
-      <Route path="student/:studentId" element={<Student />}/>
-      <Route path="listStudents" element={<StudentList/>}/>
-      </Routes>
-      </Container>
-      <Footer/>
+        <NavigationBar />
+        <MyAlert alert={alert} />
+        <Container>
+          <Routes>
+            <Route path="student" element={<Student showAlert={showAlert} />} />
+            <Route
+              path="student/:studentId"
+              element={<Student showAlert={showAlert} />}
+            />
+            <Route
+              path="listStudents"
+              element={<StudentList showAlert={showAlert} />}
+            />
+          </Routes>
+        </Container>
+        <Footer />
       </Router>
     </div>
   );
